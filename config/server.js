5b93f608d642012aef7128738dbfdaf19e2558ba
Metono <calvin.ongkingco@gmail.com>
2024-11-24 23:27:47 +0100

    updating default port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('STRAPI_HOST', '0.0.0.0'),
-  port: env.int('STRAPI_PORT', 1337),
+  port: env.int('STRAPI_PORT', 8899),
   app: {
     keys: env.array('APP_KEYS'),
   },
